cd4872310c9e9715044c1c49860e96fd69e47d75
fraanciisq <fraanciisq@gmail.com>
2025-10-23 09:41:52 +0800

    post-api

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+
+items = []
+
 @app.get("/")
-def root():
-    return {"message": "Hello, World!"}
+def get_items():
+    return {"items": items}
+
+@app.post("/")
+def add_item(item: Item):
+    items.append(item.name)
+    return {"message": f"Added {item.name}"}
